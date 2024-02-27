c284c4cc44ea08bfe4e6955032212fa1d2170ef2
chws0508 <chws0508@naver.com>
2024-02-28 01:50:20 +0900

    feat : suspendOnError를 suspendOnFailure로 수정

--- core/data/src/main/kotlin/com/withpeace/withpeace/core/data/repository/DefaultTokenRepository.kt
+++ core/data/src/main/kotlin/com/withpeace/withpeace/core/data/repository/DefaultTokenRepository.kt
@@ -1,9 +1,8 @@
 package com.withpeace.withpeace.core.data.repository
 
-import android.util.Log
-import com.skydoves.sandwich.message
+import com.skydoves.sandwich.messageOrNull
 import com.skydoves.sandwich.suspendMapSuccess
-import com.skydoves.sandwich.suspendOnError
+import com.skydoves.sandwich.suspendOnFailure
 import com.withpeace.withpeace.core.data.mapper.toDomain
 import com.withpeace.withpeace.core.datastore.dataStore.TokenPreferenceDataSource
 import com.withpeace.withpeace.core.domain.model.Token
@@ -17,54 +16,59 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
+class DefaultTokenRepository
+    @Inject
+    constructor(
+        private val tokenPreferenceDataSource: TokenPreferenceDataSource,
+        private val loginService: LoginService,
+        private val authService: AuthService,
+    ) : TokenRepository {
+        override fun getAccessToken(): Flow<String?> {
+            return tokenPreferenceDataSource.accessToken
+        }
 
-class DefaultTokenRepository @Inject constructor(
-    private val tokenPreferenceDataSource: TokenPreferenceDataSource,
-    private val loginService: LoginService,
-    private val authService: AuthService,
-) : TokenRepository {
+        override fun getRefreshToken(): Flow<String?> {
+            return tokenPreferenceDataSource.refreshToken
+        }
 
-    override fun getAccessToken(): Flow<String?> {
-        return tokenPreferenceDataSource.accessToken
-    }
+        override suspend fun updateAccessToken(accessToken: String) {
+            tokenPreferenceDataSource.updateAccessToken(accessToken)
+        }
 
-    override fun getRefreshToken(): Flow<String?> {
-        return tokenPreferenceDataSource.refreshToken
-    }
+        override suspend fun updateRefreshToken(refreshToken: String) {
+            tokenPreferenceDataSource.updateRefreshToken(refreshToken)
+        }
 
-    override suspend fun updateAccessToken(accessToken: String) {
-        tokenPreferenceDataSource.updateAccessToken(accessToken)
-    }
-
-    override suspend fun updateRefreshToken(refreshToken: String) {
-        tokenPreferenceDataSource.updateRefreshToken(refreshToken)
-    }
-
-    override suspend fun signUp(email: String, nickname: String, deviceToken: String?) {
-        authService.signUp(
-            SignUpRequest(
-                email = email,
-                nickname = nickname,
-                deviceToken = deviceToken,
+        override suspend fun signUp(
+            email: String,
+            nickname: String,
+            deviceToken: String?,
+        ) {
+            authService.signUp(
+                SignUpRequest(
+                    email = email,
+                    nickname = nickname,
+                    deviceToken = deviceToken,
+                ),
             )
-        )
-    }
+        }
 
-    override fun googleLogin(
-        idToken: String,
-        onError: (String?) -> Unit,
-    ): Flow<Token> = flow {
-        loginService.googleLogin(AUTHORIZATION_FORMAT.format(idToken))
-            .suspendMapSuccess {
-                emit(data.toDomain())
-                updateAccessToken(data.accessToken)
-                updateRefreshToken(data.refreshToken)
-            }.suspendOnError {
-                onError(message())
-            }
-    }.flowOn(Dispatchers.IO)
+        override fun googleLogin(
+            idToken: String,
+            onError: (String?) -> Unit,
+        ): Flow<Token> =
+            flow {
+                loginService.googleLogin(AUTHORIZATION_FORMAT.format(idToken))
+                    .suspendMapSuccess {
+                        emit(data.toDomain())
+                        updateAccessToken(data.accessToken)
+                        updateRefreshToken(data.refreshToken)
+                    }.suspendOnFailure {
+                        onError(messageOrNull)
+                    }
+            }.flowOn(Dispatchers.IO)
 
-    companion object {
-        private const val AUTHORIZATION_FORMAT = "Bearer %s"
+        companion object {
+            private const val AUTHORIZATION_FORMAT = "Bearer %s"
+        }
     }
-}
