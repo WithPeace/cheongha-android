2b35e11d59f761eb58806212054c3bb5580dab23
boogiwoogi <kimjinuk1999@gmail.com>
2024-02-28 20:09:23 +0900

    feat: 로그인 화면 구성

--- core/data/src/main/kotlin/com/withpeace/withpeace/core/data/repository/DefaultTokenRepository.kt
+++ core/data/src/main/kotlin/com/withpeace/withpeace/core/data/repository/DefaultTokenRepository.kt
@@ -17,58 +17,58 @@ import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class DefaultTokenRepository
-    @Inject
-    constructor(
-        private val tokenPreferenceDataSource: TokenPreferenceDataSource,
-        private val loginService: LoginService,
-        private val authService: AuthService,
-    ) : TokenRepository {
-        override fun getAccessToken(): Flow<String?> {
-            return tokenPreferenceDataSource.accessToken
-        }
-
-        override fun getRefreshToken(): Flow<String?> {
-            return tokenPreferenceDataSource.refreshToken
-        }
-
-        override suspend fun updateAccessToken(accessToken: String) {
-            tokenPreferenceDataSource.updateAccessToken(accessToken)
-        }
-
-        override suspend fun updateRefreshToken(refreshToken: String) {
-            tokenPreferenceDataSource.updateRefreshToken(refreshToken)
-        }
-
-        override suspend fun signUp(
-            email: String,
-            nickname: String,
-            deviceToken: String?,
-        ) {
-            authService.signUp(
-                SignUpRequest(
-                    email = email,
-                    nickname = nickname,
-                    deviceToken = deviceToken,
-                ),
-            )
-        }
-
-        override fun googleLogin(
-            idToken: String,
-            onError: (String?) -> Unit,
-        ): Flow<Token> =
-            flow {
-                loginService.googleLogin(AUTHORIZATION_FORMAT.format(idToken))
-                    .suspendMapSuccess {
-                        emit(data.toDomain())
-                        updateAccessToken(data.accessToken)
-                        updateRefreshToken(data.refreshToken)
-                    }.suspendOnFailure {
-                        onError(messageOrNull)
-                    }
-            }.flowOn(Dispatchers.IO)
-
-        companion object {
-            private const val AUTHORIZATION_FORMAT = "Bearer %s"
-        }
+@Inject
+constructor(
+    private val tokenPreferenceDataSource: TokenPreferenceDataSource,
+    private val loginService: LoginService,
+    private val authService: AuthService,
+) : TokenRepository {
+    override fun getAccessToken(): Flow<String?> {
+        return tokenPreferenceDataSource.accessToken
     }
+
+    override fun getRefreshToken(): Flow<String?> {
+        return tokenPreferenceDataSource.refreshToken
+    }
+
+    override suspend fun updateAccessToken(accessToken: String) {
+        tokenPreferenceDataSource.updateAccessToken(accessToken)
+    }
+
+    override suspend fun updateRefreshToken(refreshToken: String) {
+        tokenPreferenceDataSource.updateRefreshToken(refreshToken)
+    }
+
+    override suspend fun signUp(
+        email: String,
+        nickname: String,
+        deviceToken: String?,
+    ) {
+        authService.signUp(
+            SignUpRequest(
+                email = email,
+                nickname = nickname,
+                deviceToken = deviceToken,
+            ),
+        )
+    }
+
+    override fun googleLogin(
+        idToken: String,
+        onError: (String?) -> Unit,
+    ): Flow<Token> =
+        flow {
+            loginService.googleLogin(AUTHORIZATION_FORMAT.format(idToken))
+                .suspendMapSuccess {
+                    emit(data.toDomain())
+                    updateAccessToken(data.accessToken)
+                    updateRefreshToken(data.refreshToken)
+                }.suspendOnFailure {
+                    onError(messageOrNull)
+                }
+        }.flowOn(Dispatchers.IO)
+
+    companion object {
+        private const val AUTHORIZATION_FORMAT = "Bearer %s"
+    }
+}
--- feature/login/src/main/java/com/withpeace/withpeace/feature/login/LoginScreen.kt
+++ feature/login/src/main/java/com/withpeace/withpeace/feature/login/LoginScreen.kt
@@ -1,54 +1,95 @@
 package com.withpeace.withpeace.feature.login
 
-import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.withpeace.withpeace.googlelogin.GoogleLoginManager
-import kotlinx.coroutines.launch
+import androidx.compose.ui.unit.dp
+import com.withpeace.withpeace.core.designsystem.theme.WithpeaceTheme
+
 
 @Composable
 fun LoginScreen() {
-    val viewModel: LoginViewModel = viewModel()
-    val coroutineScope = rememberCoroutineScope()
-    val googleLoginManager = GoogleLoginManager(LocalContext.current)
-    Row(modifier = Modifier.fillMaxSize()) {
-        Button(
-            onClick = {
-                googleLoginManager.startLogin(
-                    coroutineScope,
-                    onSuccessLogin = {
-                        Log.d("woogi", "idToken: $it")
-                        viewModel.googleLogin(it)
-                    },
-                    onFailLogin = { Log.d("wooseok", it.toString()) },
-                )
-            },
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(text = "Login")
+            Spacer(modifier = Modifier.height(152.dp))
+            Image(
+                painter = painterResource(id = R.drawable.app_logo),
+                contentDescription = stringResource(R.string.app_logo_content_description),
+            )
+            Spacer(modifier = Modifier.height(40.dp))
+            Text(
+                style = WithpeaceTheme.typography.title1,
+                text = stringResource(R.string.welcome_to_withpeace),
+                textAlign = TextAlign.Center,
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(
+                style = WithpeaceTheme.typography.body,
+                text = stringResource(R.string.welcome_introduction),
+                textAlign = TextAlign.Center,
+            )
         }
         Button(
-            onClick = {
-                viewModel.signUp()
-            },
+            onClick = { /*TODO*/ },
+            contentPadding = PaddingValues(0.dp),
+            modifier = Modifier
+                .padding(
+                    bottom = 40.dp,
+                    end = WithpeaceTheme.padding.BasicHorizontalPadding,
+                    start = WithpeaceTheme.padding.BasicHorizontalPadding,
+                )
+                .fillMaxWidth(),
+            border = BorderStroke(width = 1.dp, color = WithpeaceTheme.colors.SystemBlack),
+            colors = ButtonDefaults.buttonColors(containerColor = WithpeaceTheme.colors.SystemWhite),
+            shape = RoundedCornerShape(9.dp),
         ) {
-            Text(text = "signup")
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                Image(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .align(Alignment.CenterStart)
+                        .size(24.dp),
+                    painter = painterResource(id = R.drawable.img_google_logo),
+                    contentDescription = stringResource(R.string.image_google_logo),
+                )
+                Text(
+                    modifier = Modifier.align(Alignment.Center),
+                    color = WithpeaceTheme.colors.SystemBlack,
+                    style = WithpeaceTheme.typography.notoSans,
+                    text = stringResource(R.string.login_to_google),
+                )
+            }
         }
     }
 }
 
-@Preview(widthDp = 400, heightDp = 900)
+@Preview(widthDp = 400, heightDp = 900, showBackground = true)
 @Composable
 fun LoginScreenPreview() {
     LoginScreen()
