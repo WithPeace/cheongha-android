7c83035cd25428f947e6a6d9994caf0cf9fe0218
kwakjoohyeong <rhkrwngud445@gmail.com>
2024-03-12 02:35:03 +0900

    refactor: googleLogin Response 수정

---
+++ core/network/src/main/java/com/withpeace/withpeace/core/network/di/response/GoogleLoginResponse.kt
@@ -0,0 +1,11 @@
+package com.withpeace.withpeace.core.network.di.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GoogleLoginResponse(
+    @SerialName("jwtTokenDto")
+    val tokenResponse: TokenResponse,
+    val role: String,
+)
