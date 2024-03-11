000c9690bc9394ce2572bba0b6c35a8fb259de3d
Covy <rhkrwngud445@gmail.com>
2024-03-12 02:48:06 +0900

    refactor: googleLogin Response 수정 (#50)
    * refactor: googleLogin Response 수정
    
    * chore: response naming 변경

--- core/network/src/main/java/com/withpeace/withpeace/core/network/di/service/AuthService.kt
+++ core/network/src/main/java/com/withpeace/withpeace/core/network/di/service/AuthService.kt
@@ -2,6 +2,7 @@ package com.withpeace.withpeace.core.network.di.service
 
 import com.skydoves.sandwich.ApiResponse
 import com.withpeace.withpeace.core.network.di.response.BaseResponse
+import com.withpeace.withpeace.core.network.di.response.LoginResponse
 import com.withpeace.withpeace.core.network.di.response.TokenResponse
 import retrofit2.http.Header
 import retrofit2.http.POST
@@ -12,7 +13,7 @@ interface AuthService {
     suspend fun googleLogin(
         @Header("Authorization")
         idToken: String,
-    ): ApiResponse<BaseResponse<TokenResponse>>
+    ): ApiResponse<BaseResponse<LoginResponse>>
 
     @POST("/api/v1/auth/refresh")
     suspend fun refreshAccessToken(): ApiResponse<BaseResponse<TokenResponse>>
