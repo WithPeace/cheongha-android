fd83a31f0e558d5d234025e5b992ef3af219b38b
kwakjoohyeong <rhkrwngud445@gmail.com>
2024-03-12 02:47:01 +0900

    chore: response naming 변경

--- core/network/src/main/java/com/withpeace/withpeace/core/network/di/response/LoginResponse.kt
+++ core/network/src/main/java/com/withpeace/withpeace/core/network/di/response/LoginResponse.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class GoogleLoginResponse(
+data class LoginResponse(
     @SerialName("jwtTokenDto")
     val tokenResponse: TokenResponse,
     val role: String,
