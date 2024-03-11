a8116d68d23609650c2791d0db4754505c2a9a5b
kwakjoohyeong <rhkrwngud445@gmail.com>
2024-03-11 16:34:13 +0900

    refactor: nullable하게 변경

--- core/domain/src/main/java/com/withpeace/withpeace/core/domain/model/WithPeaceError.kt
+++ core/domain/src/main/java/com/withpeace/withpeace/core/domain/model/WithPeaceError.kt
@@ -1,5 +1,5 @@
 package com.withpeace.withpeace.core.domain.model
 sealed interface WithPeaceError {
-    data class GeneralError(val code: Int?, val message: String?): WithPeaceError
-    data class UnAuthorized(val code: Int?, val message: String?) : WithPeaceError
+    data class GeneralError(val code: Int? = null, val message: String? = null) : WithPeaceError
+    data class UnAuthorized(val code: Int? = null, val message: String? = null) : WithPeaceError
 }
