ac59d404c1a2cb53bd6c12fbed04ff4ba0ca6930
chws0508 <chws0508@naver.com>
2024-03-12 16:12:53 +0900

    feat : StringResource 리팩토링

--- core/imagestorage/src/main/java/com/withpeace/withpeace/core/imagestorage/ImageFolder.kt
+++
@@ -1,9 +0,0 @@
-package com.withpeace.withpeace.core.imagestorage
-
-import android.net.Uri
-
-data class ImageFolderEntity(
-    val folderName: String,
-    val representativeImageUri: Uri,
-    val count: Int,
-)
