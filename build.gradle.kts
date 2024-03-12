64f870f39152e7797059b25180de260d59c3c060
chws0508 <chws0508@naver.com>
2024-03-12 16:40:27 +0900

    feat : Dependency Graph 모듈 볼수 있는 플러그인 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.dependency.graph) apply true
 }
