262e11fe2f94bf5b98289acb1662dd2f11e36ab8
chws0508 <chws0508@naver.com>
2024-03-22 20:47:16 +0900

    feat : 게시글 상세 정보 가져오는 API viewmodel에 적용

--- feature/postdetail/src/main/java/com/withpeace/withpeace/feature/postdetail/PostDetailViewModel.kt
+++ feature/postdetail/src/main/java/com/withpeace/withpeace/feature/postdetail/PostDetailViewModel.kt
@@ -2,49 +2,28 @@ package com.withpeace.withpeace.feature.postdetail
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import com.withpeace.withpeace.core.domain.model.date.Date
-import com.withpeace.withpeace.core.domain.model.post.PostContent
-import com.withpeace.withpeace.core.domain.model.post.PostDetail
-import com.withpeace.withpeace.core.domain.model.post.PostTitle
-import com.withpeace.withpeace.core.domain.model.post.PostTopic
-import com.withpeace.withpeace.core.domain.model.post.PostUser
+import androidx.lifecycle.viewModelScope
+import com.withpeace.withpeace.core.domain.usecase.GetPostDetailUseCase
 import com.withpeace.withpeace.feature.postdetail.navigation.POST_DETAIL_ID_ARGUMENT
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
-import java.time.LocalDateTime
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class PostDetailViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
+    private val getPostDetailUseCase: GetPostDetailUseCase,
 ) : ViewModel() {
     private val postId =
         checkNotNull(savedStateHandle.get<Long>(POST_DETAIL_ID_ARGUMENT)) { "게시글 아이디가 유효하지 않아요" }
 
-    private val _postUiState = MutableStateFlow<PostDetailUiState>(PostDetailUiState.Loading)
-    val postUiState = _postUiState.asStateFlow()
-
-    init {
-        _postUiState.update {
-            PostDetailUiState.Success(
-                PostDetail(
-                    user = PostUser(
-                        id = 7947,
-                        name = "Elaine Benton",
-                        profileImageUrl = "https://duckduckgo.com/?q=dictas",
-                    ),
-                    id = 9350,
-                    title = PostTitle(value = "laoreet"),
-                    content = PostContent(value = "turpis"),
-                    postTopic = PostTopic.INFORMATION,
-                    imageUrls = listOf(),
-                    createDate = Date(date = LocalDateTime.now()),
-                ),
-            )
-        }
-    }
-
-
+    val postUiState = flow {
+        getPostDetailUseCase(
+            postId,
+            onError = { emit(PostDetailUiState.Fail) },
+        ).onEach { emit(PostDetailUiState.Success(it)) }
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), PostDetailUiState.Loading)
 }
