4d1c73c24068908ced9a179c8a2c0a00010b7ca4
kwakjoohyeong <rhkrwngud445@gmail.com>
2024-03-29 23:22:31 +0900

    faet: domain nickname format exception 추가

--- core/domain/src/main/java/com/withpeace/withpeace/core/domain/model/profile/Nickname.kt
+++ core/domain/src/main/java/com/withpeace/withpeace/core/domain/model/profile/Nickname.kt
@@ -1,13 +1,17 @@
 package com.withpeace.withpeace.core.domain.model.profile
 
 /*
-    Data와 UI에서 모두 쓰일 수 있는 도메인입니다.
+    Data와 Presentation Layer에서 모두 쓰일 수 있는 도메인입니다.
  */
 @JvmInline
 value class Nickname private constructor(val value: String) {
+    init {
+        check(verifyNickname(value)) { NICKNAME_FORMAT_ERROR }
+    }
 
     companion object {
         private const val NICKNAME_REGEX_PATTERN = "[a-zA-Z0-9가-힣]{2,10}"
+        private const val NICKNAME_FORMAT_ERROR = "닉네임 형식이 잘못 되었습니다."
         fun create(value: String): Nickname = Nickname(value)
         fun verifyNickname(nickname: String): Boolean {
             val regex = Regex(NICKNAME_REGEX_PATTERN)
--- feature/mypage/src/main/java/com/withpeace/withpeace/feature/mypage/uistate/MyPageUiState.kt
+++ feature/mypage/src/main/java/com/withpeace/withpeace/feature/mypage/uistate/MyPageUiState.kt
@@ -1,7 +1,5 @@
 package com.withpeace.withpeace.feature.mypage.uistate
 
-import com.withpeace.withpeace.core.domain.model.profile.ProfileInfo
-
 sealed interface MyPageUiState {
     data object Loading : MyPageUiState
     data class Success(val profileInfo: ProfileInfoUiModel) : MyPageUiState
