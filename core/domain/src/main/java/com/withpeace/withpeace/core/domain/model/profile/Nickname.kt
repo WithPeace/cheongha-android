1958090db6cf6b238c7b206b0ee70b8f3e2f1326
kwakjoohyeong <rhkrwngud445@gmail.com>
2024-03-31 16:14:53 +0900

    refactor: nickname check(require) 제거

--- core/domain/src/main/java/com/withpeace/withpeace/core/domain/model/profile/Nickname.kt
+++ core/domain/src/main/java/com/withpeace/withpeace/core/domain/model/profile/Nickname.kt
@@ -5,13 +5,9 @@ package com.withpeace.withpeace.core.domain.model.profile
  */
 @JvmInline
 value class Nickname private constructor(val value: String) {
-    init {
-        check(verifyNickname(value)) { NICKNAME_FORMAT_ERROR }
-    }
 
     companion object {
         private const val NICKNAME_REGEX_PATTERN = "[a-zA-Z0-9가-힣]{2,10}"
-        private const val NICKNAME_FORMAT_ERROR = "닉네임 형식이 잘못 되었습니다."
         fun create(value: String): Nickname = Nickname(value)
         fun verifyNickname(nickname: String): Boolean {
             val regex = Regex(NICKNAME_REGEX_PATTERN)
