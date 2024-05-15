da03de292c965ffef9d2c0702ffaa5ee8cdc692b
kwakjoohyeong <rhkrwngud445@gmail.com>
2024-05-15 20:49:55 +0900

    chore: version code 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
     defaultConfig {
         applicationId = "com.withpeace.withpeace"
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
