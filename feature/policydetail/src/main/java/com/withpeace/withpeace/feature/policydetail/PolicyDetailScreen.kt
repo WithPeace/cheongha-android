6ebaa009b08b3b4a48853ac56df351e06b01b0e6
kwakjoohyeong <rhkrwngud445@gmail.com>
2024-06-01 21:08:39 +0900

    feat: padding 수정

--- feature/policydetail/src/main/java/com/withpeace/withpeace/feature/policydetail/PolicyDetailScreen.kt
+++ feature/policydetail/src/main/java/com/withpeace/withpeace/feature/policydetail/PolicyDetailScreen.kt
@@ -17,7 +17,7 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onGloballyPositioned
@@ -59,7 +59,7 @@ fun PolicyDetailScreen(
 ) {
     val scrollState = rememberScrollState()
     val position = remember {
-        mutableStateOf(0)
+        mutableIntStateOf(0)
     }
     val visibility = remember {
         derivedStateOf {
@@ -315,5 +315,3 @@ fun PolicyDetailPreview() {
         }
     }
 }
-
-//TODO caption 행간 확인
