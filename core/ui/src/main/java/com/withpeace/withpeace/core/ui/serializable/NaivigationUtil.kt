ae49ac67daa2344ec20444bc6c9b73a00384e471
kwakjoohyeong <rhkrwngud445@gmail.com>
2024-06-01 17:56:12 +0900

    fix: error 수정

--- core/ui/src/main/java/com/withpeace/withpeace/core/ui/serializable/NaivigationUtil.kt
+++ core/ui/src/main/java/com/withpeace/withpeace/core/ui/serializable/NaivigationUtil.kt
@@ -6,8 +6,11 @@ import java.net.URLDecoder
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 
-inline fun <reified T> T.toNavigationValue(): String =
-    URLEncoder.encode(Json.encodeToString(this), "UTF-8")
+inline fun <reified T> T.toNavigationValue(): String {
+    val json = Json.encodeToString(this)
+    return URLEncoder.encode(json.replace("%","%25"), "UTF-8")
+}
+
 
 inline fun <reified T> String.parseNavigationValue(): T {
     val decodedString = URLDecoder.decode(this, "UTF-8")
