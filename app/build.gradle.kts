107ce364cd5b31dcb55e6f76f84f87fc29a5e984
kwakjoohyeong <rhkrwngud445@gmail.com>
2024-06-18 02:44:14 +0900

    chore: 버전 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.withpeace.withpeace"
         targetSdk = 34
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
