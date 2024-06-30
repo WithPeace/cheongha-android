2518ebda89a443b3d361609539018cd94912cc66
kwakjoohyeong <rhkrwngud445@gmail.com>
2024-06-30 17:07:53 +0900

    fix: 플레이스토어 update 로직 제외

--- app/src/main/java/com/withpeace/withpeace/MainActivity.kt
+++ app/src/main/java/com/withpeace/withpeace/MainActivity.kt
@@ -40,20 +40,19 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var analyticsHelper: AnalyticsHelper
 
-    private val appUpdateManager by lazy { AppUpdateManagerFactory.create(this) }
-
-    private val updateActivityResultLauncher =
-        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result: ActivityResult ->
-            if (result.resultCode != RESULT_OK) {
-                finish()
-            }
-        }
+    // private val appUpdateManager by lazy { AppUpdateManagerFactory.create(this) }
+    //
+    // private val updateActivityResultLauncher =
+    //     registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result: ActivityResult ->
+    //         if (result.resultCode != RESULT_OK) {
+    //             finish()
+    //         }
+    //     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // System Bar에 가려지는 뷰 영역을 개발자가 제어하겠다.
         WindowCompat.setDecorFitsSystemWindows(window, false)
-
         lifecycleScope.launch {
             splashScreen = installSplashScreen()
             splashScreen.setKeepOnScreenCondition { true }
@@ -63,9 +62,7 @@ class MainActivity : ComponentActivity() {
                     when (uiState) {
                         MainUiState.Home -> composeStart(HOME_ROUTE)
                         MainUiState.Login -> composeStart(LOGIN_ROUTE)
-                        MainUiState.Update -> {
-                            compulsionUpdate()
-                        }
+                        MainUiState.Update -> {}
 
                         MainUiState.Error -> finish()
                         MainUiState.Loading -> {}
@@ -87,32 +84,32 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-    private fun compulsionUpdate() {
-        val appUpdateInfoTask: Task<AppUpdateInfo> = appUpdateManager.appUpdateInfo
-
-        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
-            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
-            ) {
-                startUpdateFlow(appUpdateManager, appUpdateInfo)
-            } else {
-                Toast.makeText(this, "앱을 사용하려면 업데이트가 필요해요!", Toast.LENGTH_LONG).show()
-                redirectToPlayStore()
-            }
-        }
-    }
-
-    private fun startUpdateFlow(appUpdateManager: AppUpdateManager, appUpdateInfo: AppUpdateInfo) {
-        try {
-            appUpdateManager.startUpdateFlowForResult(
-                appUpdateInfo,
-                updateActivityResultLauncher,
-                AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE).build(),
-            )
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
+    // private fun compulsionUpdate() {
+    //     val appUpdateInfoTask: Task<AppUpdateInfo> = appUpdateManager.appUpdateInfo
+    //
+    //     appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+    //         if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+    //             && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+    //         ) {
+    //             startUpdateFlow(appUpdateManager, appUpdateInfo)
+    //         } else {
+    //             Toast.makeText(this, "앱을 사용하려면 업데이트가 필요해요!", Toast.LENGTH_LONG).show()
+    //             redirectToPlayStore()
+    //         }
+    //     }
+    // }
+    //
+    // private fun startUpdateFlow(appUpdateManager: AppUpdateManager, appUpdateInfo: AppUpdateInfo) {
+    //     try {
+    //         appUpdateManager.startUpdateFlowForResult(
+    //             appUpdateInfo,
+    //             updateActivityResultLauncher,
+    //             AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE).build(),
+    //         )
+    //     } catch (e: Exception) {
+    //         e.printStackTrace()
+    //     }
+    // }
 
     private fun redirectToPlayStore() {
         val packageName = packageName
@@ -129,22 +126,22 @@ class MainActivity : ComponentActivity() {
         finish()
     }
 
-    override fun onResume() {
-        super.onResume()
-
-        appUpdateManager
-            .appUpdateInfo
-            .addOnSuccessListener { appUpdateInfo ->
-                if (appUpdateInfo.updateAvailability()
-                    == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS
-                ) {
-                    // If an in-app update is already running, resume the update.
-                    appUpdateManager.startUpdateFlowForResult(
-                        appUpdateInfo,
-                        updateActivityResultLauncher,
-                        AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE).build(),
-                    )
-                }
-            }
-    }
+    // override fun onResume() {
+    //     super.onResume()
+    //
+    //     appUpdateManager
+    //         .appUpdateInfo
+    //         .addOnSuccessListener { appUpdateInfo ->
+    //             if (appUpdateInfo.updateAvailability()
+    //                 == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS
+    //             ) {
+    //                 // If an in-app update is already running, resume the update.
+    //                 appUpdateManager.startUpdateFlowForResult(
+    //                     appUpdateInfo,
+    //                     updateActivityResultLauncher,
+    //                     AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE).build(),
+    //                 )
+    //             }
+    //         }
+    // }
 }
