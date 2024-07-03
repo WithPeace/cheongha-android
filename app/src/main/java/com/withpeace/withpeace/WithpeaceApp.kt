972e3077bbd693f416b10d8cf5013ce1410930c3
kwakjoohyeong <rhkrwngud445@gmail.com>
2024-07-04 01:57:59 +0900

    feat: 스낵바 구현

--- app/src/main/java/com/withpeace/withpeace/WithpeaceApp.kt
+++ app/src/main/java/com/withpeace/withpeace/WithpeaceApp.kt
@@ -1,32 +1,25 @@
 package com.withpeace.withpeace
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTagsAsResourceId
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.withpeace.withpeace.core.designsystem.theme.WithpeaceTheme
+import com.withpeace.withpeace.core.designsystem.ui.snackbar.CheonghaSnackbar
 import com.withpeace.withpeace.core.designsystem.ui.snackbar.SnackbarState
 import com.withpeace.withpeace.core.designsystem.ui.snackbar.SnackbarType
 import com.withpeace.withpeace.navigation.WithpeaceNavHost
@@ -81,22 +74,7 @@ fun WithpeaceApp(
                 when (snackBarState.snackbarType) {
                     is SnackbarType.Navigator -> {}
                     is SnackbarType.Normal -> {
-                        Row(
-                            modifier = Modifier
-                                .padding(horizontal = 24.dp)
-                                .padding(bottom = 16.dp)
-                                .imePadding()
-                                .fillMaxWidth()
-                                .clip(RoundedCornerShape(4.dp))
-                                .background(WithpeaceTheme.colors.SnackbarBlack),
-                        ) {
-                            Text(
-                                modifier = Modifier.padding(vertical = 16.dp, horizontal = 24.dp),
-                                style = WithpeaceTheme.typography.caption,
-                                text = it.visuals.message,
-                                color = WithpeaceTheme.colors.SystemWhite,
-                            )
-                        }
+                        CheonghaSnackbar(data = it)
                     }
                 }
             }
