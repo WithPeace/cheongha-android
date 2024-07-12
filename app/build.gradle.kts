8dbaf0390d44fcf844388dfb0feea8a6a098db1a
kwakjoohyeong <rhkrwngud445@gmail.com>
2024-07-12 18:14:43 +0900

    chore: 버전업

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
     defaultConfig {
         applicationId = "com.withpeace.withpeace"
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.1.0"
+        versionCode = 9
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
