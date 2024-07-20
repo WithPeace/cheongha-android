d615cbf736b42bdf45778f1b326d9bb7274f8b2e
kwakjoohyeong <rhkrwngud445@gmail.com>
2024-07-20 22:01:16 +0900

    feat: bookmark 버튼 수정

--- core/ui/src/main/java/com/withpeace/withpeace/core/ui/bookmark/BookmarkButton.kt
+++ core/ui/src/main/java/com/withpeace/withpeace/core/ui/bookmark/BookmarkButton.kt
@@ -4,14 +4,36 @@ import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.withpeace.withpeace.core.designsystem.theme.WithpeaceTheme
+import com.withpeace.withpeace.core.ui.R
 
 @Composable
 fun BookmarkButton(
     modifier: Modifier = Modifier,
+    isClick: Boolean = false,
 ) {
     Image(
         modifier = modifier,
-        painter = painterResource(id = com.withpeace.withpeace.core.ui.R.drawable.ic_empty_heart),
+        painter = painterResource(
+            id = if (isClick) R.drawable.ic_heart else com.withpeace.withpeace.core.ui.R.drawable.ic_empty_heart,
+        ),
         contentDescription = "찜하기",
     )
+}
+
+@Composable
+@Preview()
+fun BookmarkButtonPreview() {
+    WithpeaceTheme {
+        BookmarkButton()
+    }
+}
+
+@Composable
+@Preview()
+fun BookmarkButtonClickedPreview() {
+    WithpeaceTheme {
+        BookmarkButton(isClick = true)
+    }
 }
