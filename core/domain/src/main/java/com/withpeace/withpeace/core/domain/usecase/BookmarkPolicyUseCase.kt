61ab12e11407382756a0491511ca514157324cac
kwakjoohyeong <rhkrwngud445@gmail.com>
2024-08-07 19:23:46 +0900

    feat: flow first 에러 수정

--- core/domain/src/main/java/com/withpeace/withpeace/core/domain/usecase/BookmarkPolicyUseCase.kt
+++ core/domain/src/main/java/com/withpeace/withpeace/core/domain/usecase/BookmarkPolicyUseCase.kt
@@ -4,20 +4,20 @@ import com.withpeace.withpeace.core.domain.model.error.CheonghaError
 import com.withpeace.withpeace.core.domain.model.policy.BookmarkInfo
 import com.withpeace.withpeace.core.domain.repository.YouthPolicyRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class BookmarkPolicyUseCase @Inject constructor(
     private val policyRepository: YouthPolicyRepository,
 ) {
-    suspend operator fun invoke(
+    operator fun invoke(
         policyId: String,
         isBookmarked: Boolean,
         onError: suspend (CheonghaError) -> Unit,
     ): Flow<BookmarkInfo> {
-        if(isBookmarked) policyRepository.bookmarkPolicy(policyId,onError).first()
-        else policyRepository.unBookmarkPolicy(policyId, onError).first()
-        return flow { emit(BookmarkInfo(policyId, isBookmarked))}
+        return if (isBookmarked) policyRepository.bookmarkPolicy(policyId, onError)
+            .map { BookmarkInfo(id = policyId, isBookmarked = true) }
+        else policyRepository.unBookmarkPolicy(policyId, onError)
+            .map { BookmarkInfo(id = policyId, isBookmarked = false) }
     }
 }
