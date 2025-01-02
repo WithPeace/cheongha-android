888aaa81c0e9a646730bc5d23f506d7994275ae9
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-01-02 23:58:05 +0900

    feat: balance game 파일 생성

---
+++ feature/balancegame/src/main/java/com/withpeace/withpeace/feature/balancegame/BalanceGameScreen.kt
@@ -0,0 +1,13 @@
+package com.withpeace.withpeace.feature.balancegame
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BalanceGameRoute(
+) {
+    BalanceGameScreen()
+}
+
+@Composable
+fun BalanceGameScreen() {
+}
